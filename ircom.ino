4e4903c3d075d030b4b95120a95662d5adc5c316
umatbro <umatbroo@gmail.com>
2016-10-22 23:20:46 +0200

    Add files via upload

---
+++ ircom.ino
@@ -0,0 +1,19 @@
+#include <IRremote.h>
+const char irPin = 10;
+IRrecv irrecv(irPin);
+decode_results results; 
+
+void setup() {
+  Serial.begin(9600);
+  irrecv.enableIRIn();
+}
+
+void loop() {
+	if(irrecv.decode(&results))
+	{
+		Serial.print("0x");
+		Serial.println(results.value,HEX);
+		delay(250);
+		irrecv.resume();
+	}
+}
