c4587b7e938899f85e8ecf423b583e6f63093528
umatbro <umatbroo@gmail.com>
2018-07-23 20:01:40 +0200

    bt strip

---
+++ led_strip_controlled_with_bt/led_strip_controlled_with_bt.ino
@@ -0,0 +1,133 @@
+#include <Adafruit_NeoPixel.h>
+#include <SoftwareSerial.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+#define PIN 9
+#define NUM_LEDS 37
+#define BT_TX_PIN 2
+#define BT_RX_PIN 3
+
+SoftwareSerial btSerial(BT_RX_PIN, BT_TX_PIN);
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = Arduino pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRB + NEO_KHZ800);
+
+// IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
+// pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
+// and minimize distance between Arduino and first pixel.  Avoid connecting
+// on a live circuit...if you must, connect GND first.
+
+void setup() {
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+  btSerial.begin(9600);
+}
+
+char mode = '1';
+
+void loop() {
+  // Some example procedures showing how to display to the pixels:
+  // colorWipe(strip.Color(255, 0, 0), 50); // Red
+  // colorWipe(strip.Color(0, 255, 0), 50); // Green
+  // colorWipe(strip.Color(0, 0, 255), 50); // Blue
+
+  // rainbow(20);
+  if (btSerial.available() > 0) {
+    char reading = btSerial.read();
+    if (reading != '\n' && reading !=  '\r') {
+      mode = reading;
+      btSerial.print("Setting mode: ");
+      btSerial.println(mode); 
+    } else  { // ignore newline and carriage return signs
+      // btSerial.println("CR or NL");
+    }
+  }
+
+  switch (mode) {
+    case '0':
+      off();
+      break;
+    case '1':
+      rainbowCycle(20);
+      break;
+    case '2':
+      colorWipe(0xff4500, 20);
+      break;
+    default:
+      colorWipe(0xffffff, 20);
+      break;
+  }
+}
+
+// turn all pixels off
+void off() {
+  for (uint16_t i=0; i < strip.numPixels(); i++) {
+    strip.setPixelColor(i, 0, 0, 0);
+  }
+  strip.show();
+}
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  // dim  all pixels
+  for (uint16_t i=0; i < strip.numPixels(); i++) {
+    strip.setPixelColor(i, 0, 0, 0);
+  }
+  strip.show();
+
+  // sequentially show all pixels
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
+}
+
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) { // cycles of all colors on wheel
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
